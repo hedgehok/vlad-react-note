9bc37a58027f20df9698a72169fffd3c62802453
Muravev Aleksandr <AMuravev@mont.com>
2020-09-24 17:52:42 +0300

    Компонент алерта + контекст

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import { AlertContext } from '../context/alert/alertContext';
 
 export const Form = () => {
+    const [value, setValue] = useState('')
+    const alert = useContext(AlertContext)
+
+    const submitHandler = event => {
+        event.preventDefault()
+
+        if (value.trim()) {
+
+            // ... 
+
+            alert.show('Заметка была создана', 'success')
+            setValue('')
+        } else {
+            alert.show('Введите название заметки')
+        }
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="form-group">
                 <input
                     type="text"
                     className="form-control"
-                    placeholder="Введите название" />
+                    placeholder="Введите название"
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                />
             </div>
         </form>
     )
